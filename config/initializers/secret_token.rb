770f832148cd3578546935d15c4a1c0db5336174
Eduardo Reche <edu.reche@gmail.com>
2012-05-07 21:04:51 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Irun::Application.config.secret_token = '779f8e90d4df4e25221edcad31b8788f416e62e5b57f8fc2681847c7d4668b53a95bfa4cac146cc4bd92d5ac150fe13023595cc10577137ab85260c47712a296'
